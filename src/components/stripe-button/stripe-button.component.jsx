04614e20dfe8321a10559767a242117ec59991a7
Arnoldo <arnoldo.cortez@theksquaregroup.com>
2021-10-13 16:29:44 -0500

    useContext api

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey =
+    "pk_test_51JfRt6JRE4qQc6WNRWgQC8pkYdLsH2oLhlSpJ7jzlG0sx6hKlJyk1stCaJs2jGksgfHeJ8mTOQzXARQnXM2lX4XH00h79EnMKW";
 
-  const onToken = token => {
+  const onToken = (token) => {
     console.log(token);
-    alert('Payment Succesful!');
+    alert("Payment Succesful!");
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyC3Bfw0T5un5aYDx0KSLpuKjRS1STf6j5A",
+  authDomain: "crwn-db-7c887.firebaseapp.com",
+  projectId: "crwn-db-7c887",
+  storageBucket: "crwn-db-7c887.appspot.com",
+  messagingSenderId: "799463044357",
+  appId: "1:799463044357:web:0ae7c50b604235cdf6736c",
+  measurementId: "G-VQW8LF80QN",
 };
 
 firebase.initializeApp(config);
@@ -29,10 +29,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -43,7 +43,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
